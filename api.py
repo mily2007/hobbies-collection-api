6cfb6b79edd76e49d01af166f0ffb8f9f598dbad
Mily2007 <jamilynarcizo742@gmail.com>
2024-09-03 12:06:24 -0300

    banco de dados aula 03/09

--- api.py
+++ api.py
@@ -1,6 +1,19 @@
 from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///livros.db'
+
+db = SQLAlchemy(app)
+
+class Livro(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String(100),nullable=False)
+    autor = db.Column(db.String(100),nullable=False)
+
+with app.app_context():
+    db.create_all()
 
 livros = [
     {'id': 1, 'titulo': '1984', 'autor': 'George Orwell'},
@@ -10,21 +23,34 @@ livros = [
 
 @app.route('/api/livros', methods=['GET'])
 def get_livros():
-    return jsonify(livros)
+    livros = Livro.query.all()
+    return jsonify([{
+        "id": livro.id,
+        "titulo": livro.titulo,
+        "autor": livro.autor
+    } for livro in livros])
 
 @app.route('/api/livros/<int:id>', methods=['GET'])
 def get_livro(id):
-    livro = next(
-        (livro for livro in livros if livro['id'] == id),
-        None
-    )
-    return jsonify(livro) if livro else ('', 404)
+    livro = Livro.query.get(id)
+
+    return jsonify({
+        "id": livro.id,
+        "titulo": livro.titulo,
+        "autor": livro.autor
+    }) if livro else ('', 404)
 
 @app.route('/api/livros', methods=['POST'])
 def add_livro():
-    novo_livro = request.get_json()
-    livros.append(novo_livro)
-    return jsonify(novo_livro), 201
+    dados = request.get_json()
+    novo_livro = Livro(titulo=dados['titulo'], autor=dados['autor'])
+    db.session.add(novo_livro)
+    db.session.commit()
+    return jsonify({
+        "id": novo_livro.id,
+        "titulo": novo_livro.titulo,
+        "autor": novo_livro.autor
+    }), 201
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
